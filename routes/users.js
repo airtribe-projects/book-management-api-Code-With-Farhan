042acf04a8f1e28080059ac3e9d173acce4c44fd
rockstarsmaf <79184468+rockstarsmaf@users.noreply.github.com>
2024-08-26 06:26:32 +0530

    mongoDbIntegrtion, refactor

--- routes/users.js
+++ routes/users.js
@@ -4,22 +4,32 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const users = [];
+const User = require("../models/users");
 
 router.post("/register", async(req,res) => {
-    const {userName, password, role} = req.body;
-    const userExists = users.find(x => x.userName === userName);
+    const { name, age, userName, password, role} = req.body;
+    if (!userName || !name || !age || !password) {
+        return res.status(400).json({ message: 'All fields are required except role' });
+    }
+    let userExists = await User.findOne({userName});
     if(userExists) {
         return res.status(400).json({message: "User already exists"});
     }
     const hashedPass = await bcrypt.hash(password,10);
-    users.push({userName, password: hashedPass, role});
-    res.status(201).json({message: "User successfully created"});
+    user = new User({
+      userName,
+      name,
+      age,
+      password: hashedPass,
+      role
+    });
+    const dbUser = await User.create(user);
+    res.send(dbUser);
 });
 
 router.post("/login", async(req,res) => {
     const {userName, password} = req.body;
-    const user = users.find(x => x.userName === userName);
+    const user = User.findOne({userName});
     console.log(user);
     if(!user) {
         return res.status(400).json({message: "user does not exist"});
